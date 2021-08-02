153d39b5c241c8b94940edf73cbea4911d796908
frankhart2018 <siddharthadhar.soumen@srmuniv.edu.in>
2021-08-03 00:29:21 +0530

    Add validation functions and corresponding examples

---
+++ examples/login-with-swal-validation/static/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $("#login").click(function() {
+        let data = {"username": ["empty"], "password": ["empty"]};
+        validate_and_post("/login", data, swal_ajax_post);
+    });
+
+    $("#login_with_reload").click(function() {
+        let data = {"username_with_reload": ["empty"], "password_with_reload": ["empty"]};
+        validate_and_post("/login", data, swal_ajax_post_reload);
+    });
+
+    $("#login_with_redirect").click(function() {
+        let data = {"username_with_redirect": ["empty"], "password_with_redirect": ["empty"]};
+        validate_and_post("/login-with-redirect", data, simple_ajax_post_redirect);
+    });
+
+});
